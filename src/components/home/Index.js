d9c22e107ff40b98b0bcdb0d6120897c10b88acc
Mustafa Hazim <hazim6163@gmail.com>
2021-04-01 09:53:56 +0200

    single file upload

--- src/components/home/Index.js
+++ src/components/home/Index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 // import Axios
@@ -7,6 +7,8 @@ import Axios from 'axios'
 function Index(props) {
     const { p } = props
     const getCls = (base) => { return p.getCls('home-' + base) }
+
+    const [file, setFile] = useState(null)
 
 
     // user status function get axios
@@ -35,6 +37,27 @@ function Index(props) {
         })
     }
 
+    // file upload with axios
+    const fileUpload = () => {
+        var formData = new FormData();
+
+        formData.append("file", file);
+
+        Axios.post(p.apiBase + '/user/api/file/single', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'token': p.token
+            }
+        }).then((d) => {
+            console.log(d)
+        })
+    }
+
+    const fileChanged = (e) => {
+        setFile(e.target.files[0]);
+
+    }
+
     return (
         <div className={getCls('wrapper')}>
             {/* json requests with axios */}
@@ -50,6 +73,15 @@ function Index(props) {
                 {/* status headers post request  */}
                 <div onClick={statusHeader} className={'btn1 ' + getCls('btn')}>
                     status with headers
+                </div>
+            </div>
+            {/* file requests :  */}
+            <div className={getCls('wrapper1')}>
+                {/* single file upload  */}
+                <input type="file" onChange={fileChanged} />
+                {/* submit btn */}
+                <div onClick={fileUpload} className={'btn1 ' + getCls('btn')}>
+                    single file upload
                 </div>
             </div>
         </div>
